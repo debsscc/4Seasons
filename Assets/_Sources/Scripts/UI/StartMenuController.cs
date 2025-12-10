ca8e5ea6f12a0cc233b15c5e4fadbf967a83f9e1
Debora Carvalho <dbndy3@gmail.com>
2025-12-10 17:24:22 -0300

    Config Dialogues, Settings and Menu

--- Assets/_Sources/Scripts/UI/StartMenuController.cs
+++ Assets/_Sources/Scripts/UI/StartMenuController.cs
@@ -5,9 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class StartMenuController : MonoBehaviour
 {
+    public GameObject optionsModal;
+
+    void Start()
+    {
+        if (optionsModal != null) {
+            optionsModal.SetActive(false);
+        }
+    }
+
     public void OnStartClick()
     {
         SceneManager.LoadScene("MapSeletor");
+    }
+    public void onOptionsClick()
+    {
+        Debug.Log("Options clicked");
+        if (optionsModal != null)
+        {
+            Debug.Log("Isnt null clicked");
+            optionsModal.SetActive(true);
+        }
+    }
+    public void onCloseOptionsModal()
+    {
+        Debug.Log("Close options clicked");
+        if (optionsModal != null)
+        {
+            Debug.Log("Isnt null  CLOSE clicked");
+            optionsModal.SetActive(false);
+        }
     }
 
     public void onExitClick()
@@ -18,5 +45,3 @@ public class StartMenuController : MonoBehaviour
         Application.Quit();
     }
 }
-
-
