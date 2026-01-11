958ea844045cef2b936c48d736edd49eb07aef76
cdreyer <cristianbr.dreyer@gmail.com>
2026-01-10 22:55:03 -0300

    select dialogue option done

--- Assets/_Sources/Tests/ChangeOptionHolder.cs
+++ Assets/_Sources/Tests/ChangeOptionHolder.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using Yarn.Unity;
 
 public class ChangeOptionHolder : MonoBehaviour
 {
     [SerializeField] private HorizontalLayoutGroup _layoutGroup;
     [SerializeField] private Button _leftArrowButton;
     [SerializeField] private Button _rightArrowButton;
+    [SerializeField] private Button _confirmButton;
     [SerializeField] private float _itemSize = 200f;
 
+    private DialogueRunner _dialogueRunner;
     private float _currentScrollPosition = 0f;
     private int _currentIndex = 0;
     private float _posOffset = 0f;
@@ -37,9 +40,19 @@ public class ChangeOptionHolder : MonoBehaviour
         set
         {
             _currentIndex = Mathf.Clamp(value, 0, _layoutGroup.transform.childCount - 1);
-            Debug.Log($"current post set to {_posOffset}");
-            Debug.Log($"scroll amount: {ScrollAmount}");
             CurrentLayoutPosition = -_currentIndex * ScrollAmount + _posOffset;
+        }
+    }
+
+    private OptionItem[] Options => _layoutGroup.GetComponentsInChildren<OptionItem>();
+
+    void Awake()
+    {
+        if (_dialogueRunner == null)
+        {
+            _dialogueRunner = GetComponentInParent<DialogueRunner>();
+            if(!_dialogueRunner)
+                Debug.LogError("DialogueRunner component not found in parent.");
         }
     }
 
@@ -55,7 +68,22 @@ public class ChangeOptionHolder : MonoBehaviour
             CurrentIndex++;
         });
 
+
+        _confirmButton.onClick.AddListener(ConfirmSelection);
+
         _posOffset = _layoutGroup.transform.localPosition.x;
+    }
+
+    void ConfirmSelection()
+    {
+       var options = Options;
+
+        if (options.Length > 0 && _currentIndex >= 0 && _currentIndex < options.Length)
+        {
+            var selectedOption = options[_currentIndex];
+            selectedOption.InvokeOptionSelected();
+            Debug.Log($"Selected option: {selectedOption}", selectedOption);
+        }
     }
 
 }
